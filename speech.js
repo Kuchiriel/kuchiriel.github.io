a3a70e785c6b304673f0c37a47a7d07923c5ba44
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-05 21:36:03 -0800

    Update speech.js

--- speech.js
+++ speech.js
@@ -45,7 +45,7 @@
             console.log("Engine: Loaded Speech Recognition");
         
                 annyang.start({
-                    autoRestart: true
+                    autoRestart: false
                 });
             }
         
