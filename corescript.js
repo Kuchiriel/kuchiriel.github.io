13e52eb6fa2f8f10905f839b6f10a4925a70e99d
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-09 06:30:12 -0800

    Update corescript.js

--- corescript.js
+++ corescript.js
@@ -87,8 +87,6 @@
      $(document).ready(function(){
     $.ajax({
             
-        var key = '';    
-        
         url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='
             + "trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a"
             + '&text=' + something + '&lang=en';
