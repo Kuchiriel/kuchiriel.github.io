fa81a2f00a655a6820fb2c99213e14f809c976c2
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-09 05:59:52 -0800

    Update core.js

--- core.js
+++ core.js
@@ -137,9 +137,8 @@
         }        
         function receiveReply(something) {
             console.log("You: " + something);
-            console.log(https://translate.yandex.net/api/v1.5/tr.json/detect ? 
-key=trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a
- & text=something)
+            translated = 'https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a&text=' + something;
+            console.log("Translated: " + translated);
             reply = sendMessage(something);
             showReply(reply);
         }
