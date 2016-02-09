5e639787e62e4f412dc7d5790104c348190e3166
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-09 06:31:40 -0800

    Update corescript.js

--- corescript.js
+++ corescript.js
@@ -89,7 +89,7 @@
             
         url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key='
             + "trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a"
-            + '&text=' + something + '&lang=en';
+            + '&text=' + something + '&lang=en'
 
         dataType: 'jsonp',
         success: function(json) {
