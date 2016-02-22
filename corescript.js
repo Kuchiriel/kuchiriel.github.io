a38050360b6c844bbac007101aba168a2356bfb0
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-22 08:03:09 -0300

    Update corescript.js

--- corescript.js
+++ corescript.js
@@ -99,11 +99,12 @@
        function translate(something) { 
                
        var trsapi = new XMLHttpRequest();
-       var url = https://translate.yandex.net/api/v1.5/tr.json/translate ? 
-       key = "trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a"
-       & text = something
-       & lang = "pt-en"; 
-       trsapi.open("GET", url, false);
+      
+       var key = "trnsl.1.1.20160209T133106Z.3dc9bb19cc139b13.f1065a13645d8992a5b7357812a092551edb338a";
+       var text = something;
+       var lang = "pt-en"; 
+       
+       trsapi.open("GET", "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + key + "&text" + text + "&lang" + lang, false);
        trsapi.send();
 
        console.log(trsapi.status);
