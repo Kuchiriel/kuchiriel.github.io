f915907fd5bb3ea3f67c94f9c06d3ec7cd5bd5ea
Matheus Almeida <matheus.almeida55@outlook.com>
2016-02-24 08:13:56 -0300

    Update routines.js

--- routines.js
+++ routines.js
@@ -8,3 +8,8 @@
         annyang.setLanguage(recogLanguage);
         console.log("Engine: Recognition Language Set To " + recogLanguage)
         });
+        
+     rs.setSubroutine('toggleSpeechLanguage', function(rs, args) {
+        outputLanguage = args;
+        console.log("Engine: Speech Language Set To " + outputLanguage)
+        });    
