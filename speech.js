bc4e4bf95bf224f3d3bb935a59dcf82cba695333
Matheus Almeida <matheus.almeida55@outlook.com>
2016-04-10 18:12:01 -0300

    Update speech.js

--- speech.js
+++ speech.js
@@ -1,55 +1,54 @@
             if (annyang) {
 
-               annyang.setLanguage(recogLanguage);
-      
-                var botCommand = {
-                    '(hey) Kia *something': recogFunction
-                };
-                var commandBot = {
-                    '(hey) *something Kia': recogFunction
-                };
-                var commandToggle = {
-                    '(hey) Kia (hey)': tempRec
-                };
-                var tempCommand = {
-                    '*something': tempSend
-                };
-                var consoleDebug = {
-                    '*debug': function(debug) {
-                        console.log("Debug: " + debug);
-                    }
-                };
-            
+             annyang.setLanguage(recogLanguage);
 
-                function recogFunction(something) {
-                    receiveReply(something);
-                }
-               
+             var botCommand = {
+              '(hey) Samaritan *something': recogFunction
+             };
+             var commandBot = {
+              '(hey) *something Samaritan': recogFunction
+             };
+             var commandToggle = {
+              '(hey) Samaritan (hey)': tempRec
+             };
+             var tempCommand = {
+              '*something': tempSend
+             };
+             var consoleDebug = {
+              '*debug': function(debug) {
+               console.log("Debug: " + debug);
+              }
+             };
 
-                function tempRec() {
-                    annyang.removeCommands();
-                    receiveReply("Samaritan");
-                    annyang.addCommands(tempCommand);
-                }
 
-                function tempSend(something) {
-                    annyang.removeCommands();
-                    receiveReply(something);
-                    addCommandsBack();
-                }
-                
-            addCommandsBack();
-            console.log("Engine: Loaded Speech Recognition");
-        
-                annyang.start({
-                    autoRestart: true
-                });
+             function recogFunction(something) {
+              receiveReply(something);
+             }
+
+
+             function tempRec() {
+              annyang.removeCommands();
+              receiveReply("Samaritan");
+              annyang.addCommands(tempCommand);
+             }
+
+             function tempSend(something) {
+              annyang.removeCommands();
+              receiveReply(something);
+              addCommandsBack();
+             }
+
+             addCommandsBack();
+             console.log("Engine: Loaded Speech Recognition");
+
+             annyang.start({
+              autoRestart: true
+             });
             }
-    
-    var urlSend = getUrlParameter('send');
 
-    if (urlSend !== undefined)
-    {
-     urlSend = urlSend.split('%20').join(' ').split('%22').join('').split('%27').join("'").toString();
-     document.body.innerHTML = receiveReply(urlSend.toString());
-    }
+            var urlSend = getUrlParameter('send');
+
+            if (urlSend !== undefined) {
+             urlSend = urlSend.split('%20').join(' ').split('%22').join('').split('%27').join("'").toString();
+             document.body.innerHTML = receiveReply(urlSend.toString());
+            }
